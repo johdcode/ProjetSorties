2c2a070ffe8d012ad73b91abd01259b402b47f3e
ValentinD04 <68585092+ValentinD04@users.noreply.github.com>
2020-10-14 13:46:04 +0200

    UPG - Form Sortie
    Rename du formulaire

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SortieType extends AbstractType
+class Sortie1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -18,6 +18,7 @@ class SortieType extends AbstractType
             ->add('dateLimiteInscription')
             ->add('nbInscriptionsMax')
             ->add('infosSortie')
+            ->add('motifAnnulation')
             ->add('etat')
             ->add('lieu')
             ->add('organisateur')
