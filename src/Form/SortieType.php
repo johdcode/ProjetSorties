e7f985be94edcd2d66de29a8d1bdb9e03b25f851
ValentinD04 <68585092+ValentinD04@users.noreply.github.com>
2020-10-14 14:23:48 +0200

    UPG - SortieForm
    - Tout nouveau, tout beau

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class Sortie1Type extends AbstractType
+class SortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
