81360c7719edac3c65c3cd283bd1d26ae459187e
ValentinD04 <68585092+ValentinD04@users.noreply.github.com>
2020-10-15 12:32:35 +0200

    UPG - SortieType w/ controller and twig
    - Affichage du formulaire Lieu dans celui de la sortie
    - Mise en forme de la vue

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,8 +2,18 @@
 
 namespace App\Form;
 
+use App\Entity\Etat;
+use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Entity\Ville;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +22,36 @@ class SortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionsMax')
-            ->add('infosSortie')
-            ->add('motifAnnulation')
-            ->add('etat')
-            ->add('lieu')
-            ->add('organisateur')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie : '
+            ])
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie : '
+            ])
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée : '
+            ])
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'label' => 'Date limite d\'inscritpion : '
+            ])
+            ->add('nbInscriptionsMax', IntegerType::class, [
+                'label' => 'Nombres de places : '
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'label' => 'Description et infos : '
+            ])
+            ->add('lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('l')
+                        ->orderBy('l.nom', 'ASC');
+                },
+                'choice_label' => 'nom'
+            ])
+            ->add('lieu', LieuType::class)
+            ->add('enregistrer', SubmitType::class)
+            ->add('publier', SubmitType::class)
+            ->add('annuler', SubmitType::class)
         ;
     }
 
